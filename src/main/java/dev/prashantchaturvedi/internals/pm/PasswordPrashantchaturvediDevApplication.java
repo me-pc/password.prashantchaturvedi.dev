da1f4f2f4885d4da9e0d2bc733c962967ba8c696
Prashant Chaturvedi <prashant.chaturvedi@codapayments.com>
2022-08-26 19:25:00 +0530

    Adds initial setup for application

---
+++ src/main/java/dev/prashantchaturvedi/internals/pm/PasswordPrashantchaturvediDevApplication.java
@@ -0,0 +1,13 @@
+package dev.prashantchaturvedi.internals.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordPrashantchaturvediDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordPrashantchaturvediDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/prashantchaturvedi/internals/pm/PasswordPrashantchaturvediDevApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prashantchaturvedi.internals.pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordPrashantchaturvediDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
