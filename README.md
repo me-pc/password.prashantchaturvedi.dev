2663167b437506b8011f0f0752a3191a1c26624d
Prashant Chaturvedi <prashant.chaturvedi@codapayments.com>
2022-08-26 19:44:32 +0530

    Adds initial setup for database

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # password.prashantchaturvedi.dev
 Password manager for users
+
+In this project we are going to get started with creating a password manager for personal use. We will add multiple features and would go step by step to do that.
+Lets start by setting up database for our application using `docker-compose`
--- src/main/java/dev/prashantchaturvedi/internals/pm/PasswordPrashantchaturvediDevApplication.java
+++ src/main/java/dev/prashantchaturvedi/internals/pm/PasswordPrashantchaturvediDevApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasswordPrashantchaturvediDevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PasswordPrashantchaturvediDevApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordPrashantchaturvediDevApplication.class, args);
+    }
 
 }
